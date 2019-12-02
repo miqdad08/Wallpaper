8d50e9598fc62ecd6d5bc3909a08c07dbadacd6d
Miqdad <miqdaddeveloper@gmail.com>
2019-12-02 20:56:05 +0700

    Initial commit

---
+++ app/src/main/java/id/com/miqdad/wallpaper/ThemeActivty.java
@@ -0,0 +1,84 @@
+package id.com.miqdad.wallpaper;
+
+import android.app.WallpaperManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class ThemeActivty extends AppCompatActivity {
+
+    LinearLayout theme_preview;
+    Button btn_back, btn_save;
+    String newString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_activty);
+
+
+        btn_back=(Button)findViewById(R.id.btn_back);
+        btn_save=(Button)findViewById(R.id.btn_save);
+        theme_preview=(LinearLayout)findViewById(R.id.theme_preview);
+
+
+        if(savedInstanceState==null){
+            Bundle extras=getIntent().getExtras();
+            if(extras==null){
+                newString=null;
+            }else {
+                newString=extras.getString("PATH_PICTURE");
+                if(newString.equals("btn_1")){
+                    theme_preview.setBackgroundResource(R.drawable.bg_1);
+
+                    btn_save.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            WallpaperManager wlp=WallpaperManager.getInstance(getApplicationContext());
+                            Toast.makeText(getApplicationContext(),"Berhasil dipasang",Toast.LENGTH_SHORT).show();
+                            try{
+                                wlp.setResource(+ R.drawable.bg_1);
+                            }catch (IOException e){
+                                e.printStackTrace();
+                            }
+                        }
+
+                    });
+
+
+                }else if (newString.equals("btn_2")) {
+                    theme_preview.setBackgroundResource(R.drawable.bg_2);
+                    btn_save.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            WallpaperManager wlp=WallpaperManager.getInstance(getApplicationContext());
+                            Toast.makeText(getApplicationContext(),"Berhasil dipasang",Toast.LENGTH_SHORT).show();
+                            try{
+                                wlp.setResource(+ R.drawable.bg_2);
+                            }catch (IOException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                }else{
+                    //write the statement
+                }
+            }
+        }
+
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
